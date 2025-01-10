bc9d528838559f284aec9dc23a3554e5f63036bc
Damien Wallis <damie.grobi@gmail.com>
2025-01-10 09:18:10 +0100

    finished 05IfBedingung

---
+++ JS/05IfBedingung/script.js
@@ -0,0 +1,30 @@
+// Aufgabe 1
+let number1 = 21;
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number1 < 20) {
+  console.log("Zahl 1: Mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+else if (number1 >= 20 && number1 <= 50) {
+  console.log("Zahl 1: Medium");
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+else {
+  console.log("Zahl 1: Large");
+}
+
+// Aufgabe 2
+let number2 = 101;
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (number2 < 7) {
+  console.log("Zahl 2: Mini");
+}
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+else if (number2 >= 7 && number2 <= 90) {
+  console.log("Zahl 2: Medium");
+}
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+else {
+  console.log("Zahl 2: Large");
+}
