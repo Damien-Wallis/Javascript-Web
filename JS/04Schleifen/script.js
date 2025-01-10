7a223999210b4aa57b2beb19fef262cac1ae2ce9
Damien Wallis <damie.grobi@gmail.com>
2025-01-10 09:09:38 +0100

    finished 04Schleifen

---
+++ JS/04Schleifen/script.js
@@ -0,0 +1,36 @@
+// Zähle von 7 bis 45
+for (let count = 7; count <= 45; count++) {
+  console.log(count);
+}
+
+// Zähle von 9 bis 99
+for (count = 9; count <= 99; count++) {
+  console.log(count);
+}
+
+// Zähle rückwärts von 10 bis 0
+for (count = 10; count >= 0; count--) {
+  console.log(count);
+}
+
+// Zähle rückwärts von 1234 bis 1207
+for (count = 1234; count >= 1207; count--) {
+  console.log(count);
+}
+
+// Gib Deinen Namen genau 8 mal aus
+for (count = 0; count <= 8; count++) {
+  let name = "Damien";
+  console.log(name);
+}
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (count = 0; count <= 10; count++) {
+  console.log(count);
+}
+let name = "Damien";
+console.log(name);
+
+for (count = 10; count >= 0; count--) {
+  console.log(count);
+}
