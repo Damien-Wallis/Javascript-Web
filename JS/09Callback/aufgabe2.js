49e519c6b51e7aa4a92d6c78a2f9ae23f3aebef6
Damien Wallis <damie.grobi@gmail.com>
2025-01-17 09:44:06 +0100

    finished aufgabe2

---
+++ JS/09Callback/aufgabe2.js
@@ -0,0 +1,18 @@
+//Aufgabe 2
+
+// Die Funktion orderPizza nimmt den Namen der Pizza und einen Callback entgegen.
+// Sie simuliert das Bestellen der Pizza.
+// Wenn die Pizza fertig ist wird der Callback aufgerufen.
+function orderPizza(name, callback) {
+  console.log("Pizza " + name);
+  callback();
+}
+
+// Dem Callback wird ein zufälliger Preis mitgegeben.
+function price() {
+  let random = Math.random() * 5 + 7;
+  let randomFloor = Math.floor(random);
+  console.log("price: " + randomFloor);
+}
+
+orderPizza("Funghi", price);
