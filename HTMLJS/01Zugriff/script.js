469d6ac9258e2d6e0638b4ef5fc6b719cca4bddc
Damien Wallis <damie.grobi@gmail.com>
2025-01-31 09:30:43 +0100

    added quotient function

--- HTMLJS/01Zugriff/script.js
+++ HTMLJS/01Zugriff/script.js
@@ -14,3 +14,12 @@ function product() {
   let result = num1 * num2;
   console.log("Produkt von " + num1 + " und " + num2 + ": " + result);
 }
+
+function quotient() {
+  if (num2 != 0) {
+    let result = num1 / num2;
+    console.log("Quotient von " + num1 + " und " + num2 + ": " + result);
+  } else {
+    console.log("Division durch 0 ist nicht möglich.");
+  }
+}
