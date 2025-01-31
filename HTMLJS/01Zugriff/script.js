f32525f69be85d6ce0caf10bad917e75254decdd
Damien Wallis <damie.grobi@gmail.com>
2025-01-31 09:15:42 +0100

    added button and input fields

--- HTMLJS/01Zugriff/script.js
+++ HTMLJS/01Zugriff/script.js
@@ -1,2 +1,11 @@
-document.getElementById("content").innerHTML = "Tolle Sache";
-console.log(element);
+function buttonClicked() {
+  console.log("Button clicked!");
+  let num1 = parseFloat(document.getElementById("number1").value);
+  let num2 = parseFloat(document.getElementById("number2").value);
+  console.log(num1);
+  console.log(num2);
+
+  let result = num1 + num2;
+
+  console.log(result);
+}
