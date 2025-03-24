793676b20255d88940fba9a9f7b4612680d7dfb6
Damien Wallis <damie.grobi@gmail.com>
2025-03-24 14:57:54 +0100

    changed folder name

--- REACT/demo/demo/src/components/List.jsx
+++ REACT/demo/demo/src/components/List.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function List({ item }) {
   return (
-    <div className="bg-blue-500 border w-full">
+    <div className="bg-teal-600 border w-50 h-15">
       <p>{item}</p>
     </div>
   );
