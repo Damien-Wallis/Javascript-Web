08155e4346fe9197c36ff95f8eb403a41d100f2d
Damien Wallis <damie.grobi@gmail.com>
2025-03-24 15:28:51 +0100

    added clickable Button

--- REACT/demo/demo/src/App.jsx
+++ REACT/demo/demo/src/App.jsx
@@ -3,18 +3,23 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import List from "./components/List";
+import Button from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="">Liste</h1>
+      <h1 className="text-3xl">List</h1>
       <div className="">
         <List item={"Coffee"} />
         <List item={"Tea"} />
         <List item={"Beer"} />
       </div>
+
+      <h1 className="text-3xl">Button</h1>
+      <Button count={count} setCount={setCount} />
+      <p className="m-3">Button pressed: {count} times</p>
     </>
   );
 }
