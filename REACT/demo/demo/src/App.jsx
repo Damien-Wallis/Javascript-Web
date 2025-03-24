615b03652719ecc20713648416e3943c43d377bf
Damien Wallis <damie.grobi@gmail.com>
2025-03-24 15:58:56 +0100

    added person card

--- REACT/demo/demo/src/App.jsx
+++ REACT/demo/demo/src/App.jsx
@@ -4,12 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import List from "./components/List";
 import Button from "./components/Button";
+import Card from "./components/Card";
+import johnDoeImg from "./assets/john-doe.jpg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      {/* Liste */}
       <h1 className="text-3xl">List</h1>
       <div className="">
         <List item={"Coffee"} />
@@ -17,9 +20,18 @@ function App() {
         <List item={"Beer"} />
       </div>
 
+      {/* Button */}
       <h1 className="text-3xl">Button</h1>
       <Button count={count} setCount={setCount} />
       <p className="m-3">Button pressed: {count} times</p>
+
+      {/* Card */}
+      <h1 className="text-3xl">Card</h1>
+      <Card
+        img={johnDoeImg}
+        name={"John Doe"}
+        occupation={"Architect & Engineer"}
+      />
     </>
   );
 }
